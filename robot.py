cb6129871f966431c1211b4ffa60f1501bc2ccdf
Ignacio Bonelli <ignacio@e-condo.com>
2018-07-27 08:57:20 -0300

    Getting the world created.

---
+++ robot.py
@@ -0,0 +1,21 @@
+import math
+
+class Config():
+    # simulation parameters
+
+    def __init__(self):
+        # robot parameter
+        self.step = 1  # [m]
+        self.max_speed = 1.0  # [m/s]
+        self.min_speed = -0.5  # [m/s]
+        self.max_yawrate = 40.0 * math.pi / 180.0  # [rad/s]
+        self.max_accel = 0.2  # [m/ss]
+        self.max_dyawrate = 40.0 * math.pi / 180.0  # [rad/ss]
+        self.v_reso = 0.01  # [m/s]
+        self.yawrate_reso = 0.1 * math.pi / 180.0  # [rad/s]
+        self.dt = 0.1  # [s]
+        self.predict_time = 3.0  # [s]
+        self.to_goal_cost_gain = 1.0
+        self.speed_cost_gain = 1.0
+        self.robot_radius = 1.0  # [m]
+ 
