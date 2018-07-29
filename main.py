3d6e5fdfed7fd7e475e8502128db50aba665b302
Ignacio Bonelli <ignacio@e-condo.com>
2018-07-29 18:09:02 -0300

    Adding a second random agent and producing the output on file

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import numpy as np
 	# https://docs.scipy.org/doc/numpy/user/basics.creation.html
 import matplotlib.pyplot as plt
 	# https://matplotlib.org/users/pyplot_tutorial.html
+import subprocess
 
 # Modules
 from agent_dwa import dwaAgent
@@ -18,16 +19,25 @@ from agent_random import rAgent
 
 # Globals
 show_animation = True
+file_number = 1
 
 def main():
+    global file_number
     print(__file__ + " start!!")
-    
+
     # Random1 --------
     # initial state [x, y]
     r1_x = np.array([40, 20])
     # Saving trajectory
     r1_traj = np.array(r1_x)
     r1_ticks = 0
+
+    # Random2 --------
+    # initial state [x, y]
+    r2_x = np.array([20, 40])
+    # Saving trajectory
+    r2_traj = np.array(r2_x)
+    r2_ticks = 0
 
     # DWA ------------
     # initial state [x, y, yaw(rad), v(m/s), omega(rad/s)]
@@ -44,6 +54,7 @@ def main():
 
     dwa = dwaAgent()
     r1 = rAgent()
+    r2 = rAgent()
 
     for i in range(1000):
         # Random1 - Calculating trajectory
@@ -52,16 +63,29 @@ def main():
         else:
             r1_ticks-=1
         # Random1 - Calculating movement
-        dwa_ob = np.array([x[0], x[1], dwa.robot_radius])
-        ob4r1 = np.vstack((ob,dwa_ob))
+        ob2add = np.array([[x[0], x[1], dwa.robot_radius], [r2_x[0], r2_x[1], r2.robot_radius]])
+        ob4r1 = np.vstack((ob,ob2add))
         r1_x, r1_col = r1.motion(r1_x, ob4r1, r1_ang, r1_vel)
         if (r1_col):
             r1_ticks = 0
         r1_traj = np.vstack((r1_traj, r1_x))  # store state history
 
+        # Random2 - Calculating trajectory
+        if (r2_ticks == 0):
+            r2_ang, r2_vel, r2_ticks = r2.random_control()
+        else:
+            r2_ticks-=1
+        # Random2 - Calculating movement
+        ob2add = np.array([[x[0], x[1], dwa.robot_radius], [r1_x[0], r1_x[1], r1.robot_radius]])
+        ob4r2 = np.vstack((ob,ob2add))
+        r2_x, r2_col = r2.motion(r2_x, ob4r2, r2_ang, r2_vel)
+        if (r2_col):
+            r2_ticks = 0
+        r2_traj = np.vstack((r2_traj, r2_x))  # store state history
+
         # DWA
-        r1_ob = np.array([r1_x[0], r1_x[1], r1.robot_radius])
-        ob4dwa = np.vstack((ob,r1_ob))
+        ob2add = np.array([[r1_x[0], r1_x[1], r1.robot_radius], [r2_x[0], r2_x[1], r2.robot_radius]])
+        ob4dwa = np.vstack((ob,ob2add))
         u, ltraj = dwa.dwa_control(x, u, goal, ob4dwa)
         x = dwa.motion(x, u)
         print "Pos " + str(i) + " - x: " + str(x)
@@ -71,10 +95,12 @@ def main():
             plt.cla()
             # Random1
             plt.plot(r1_x[0], r1_x[1], "xr")
+            # Random1
+            plt.plot(r2_x[0], r2_x[1], "xb")
             # DWA
             plt.plot(ltraj[:, 0], ltraj[:, 1], "-g")
             plt.plot(x[0], x[1], "xg")
-            plt.plot(goal[0], goal[1], "xb")
+            plt.plot(goal[0], goal[1], "ob")
             # ob[:, 0] -> The full first row of the array (all X numbers)
             # ob[:, 1] -> The full second row of the array (all Y numbers)
             #plt.plot(ob[:, 0], ob[:, 1], "ok")
@@ -85,6 +111,11 @@ def main():
             dwa.plot_arrow(x[0], x[1], x[2])
             plt.axis("equal")
             plt.grid(True)
+            # We save to a file
+            imagefile = format(file_number, '05d')
+            file_number += 1
+            plt.savefig("/home/ignacio/Downloads/PyPlot/anim_" + imagefile + ".png")
+            # And show as we used to
             plt.pause(0.0001)
 
         # check goal
@@ -92,14 +123,26 @@ def main():
             print("Goal!!")
             break
 
+    path = "/home/ignacio/Downloads/PyPlot/"
+    cmd = "convert -delay 0.5 " + path + "anim_*.png -loop 0 -monitor " + path + "movie.gif"
+    #status = subprocess.call(cmd, shell=True)
+    print "First run:"
+    print "    " + cmd
+    print "And then run:"
+    print "    " + "rm " + path + "anim_*.png"
+
     print("Done")
     if show_animation:
         # Random1
         plt.plot(r1_traj[:, 0], r1_traj[:, 1], "-r")
+        # Random2
+        plt.plot(r2_traj[:, 0], r2_traj[:, 1], "-b")
         # DWA
         plt.plot(traj[:, 0], traj[:, 1], "-g")
+        # We save to a file
+        plt.savefig("/home/ignacio/Downloads/PyPlot/movie_end.png")
+        # And show as we used to
         plt.show()
-
 
 if __name__ == '__main__':
     main()
